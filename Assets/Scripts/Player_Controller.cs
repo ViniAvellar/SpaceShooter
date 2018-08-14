c32dfc83afd1b54d8cda8fa4b4bbb50bb8bd3d41
Vinocius Battistelli De Avellar <1705415@educacional.up>
2018-08-13 21:18:13 -0300

    player

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+	public float speed;
+	
+	private Rigidbody myRigidbody;
+	
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		
+		
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		
+		//myRigidbody.velocity = new Vector3(x, y, z);
+		myRigidbody.velocity = new Vector3(horizontal * speed, 0.0f, vertical * speed);
+		
+		Debug.Log("h: " + horizontal + ", v: " + vertical);
+	}
+	
+}
