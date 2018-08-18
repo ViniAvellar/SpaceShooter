1b75491e9d4722b0294cc9a2deb701e35649060f
Vinocius Battistelli De Avellar <1705415@educacional.up>
2018-08-17 21:21:09 -0300

    player movement

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 public class Player_Controller : MonoBehaviour {
 
 	public float speed;
-	
+    public float tilt;
+    public Boundary boundary;
+
 	private Rigidbody myRigidbody;
 	
 	void Start()
@@ -24,6 +31,14 @@ public class Player_Controller : MonoBehaviour {
 		myRigidbody.velocity = new Vector3(horizontal * speed, 0.0f, vertical * speed);
 		
 		Debug.Log("h: " + horizontal + ", v: " + vertical);
+
+        myRigidbody.position = new Vector3
+            (
+            Mathf.Clamp(myRigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(myRigidbody.position.z, boundary.zMin, boundary.zMax)
+            );
+        myRigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, myRigidbody.velocity.x * -tilt);
 	}
 	
 }
