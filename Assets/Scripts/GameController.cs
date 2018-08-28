4000a3653575d1ccda10ef588c4f1ca6e47667d6
ViniAvellar <viniavellar@gmail.com>
2018-08-27 21:43:49 -0300

    Spawn Wave

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 SpawnValues;
+    public int hazardCount;
+    public float SpawnWait;
+    public float StartWait;
+    public float WaveWait;
+
+	void Start ()
+    {
+        StartCoroutine(SpawnWaves());
+	}
+	
+	IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds(StartWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-5.0f, 5.0f), SpawnValues.y, SpawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(SpawnWait);
+            }
+            yield return new WaitForSeconds(WaveWait);
+        }
+    }
+}
